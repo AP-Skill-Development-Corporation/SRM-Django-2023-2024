b404c99493afb94967aa6532cb9ba0d75486205a
SRM <rravikumar34@gmail.com>
2023-07-17 17:12:16 +0530

    Day-23 Updated

---
+++ Day-23/DemoApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+	roll = models.CharField(max_length=15)
+	sname = models.CharField(max_length=30)
+	year = models.IntegerField()
+	branch = models.CharField(max_length=30)
+
